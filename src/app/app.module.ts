6df6985612518f3691c781159a19aad5ad272bbb
KushalBiyani <KushalBiyani@gmail.com>
2020-08-25 20:24:43 +0530

    2-3-11 assignment3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { FormsModule } from '@angular/forms';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatSliderModule } from '@angular/material/slider';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import 'hammerjs';
 
@@ -62,6 +63,7 @@ import { MatDialogModule } from '@angular/material/dialog';
     MatInputModule,
     MatProgressSpinnerModule,
     MatCheckboxModule,
+    MatSliderModule,
     FlexLayoutModule,
     FormsModule
   ],
