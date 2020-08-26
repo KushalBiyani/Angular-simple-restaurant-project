84862c2f03cd1aded7950f7f320e1b9478595563
KushalBiyani <KushalBiyani@gmail.com>
2020-08-26 19:40:15 +0530

    2-4-6 Animations Part 2

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,11 +1,19 @@
 import {Component , Inject , OnInit} from '@angular/core';
 import {Dish} from '../shared/dish';
-
+import { flyInOut , expand } from '../animations/app.animation';
 import {DishService} from '../services/dish.service';
 @Component({
   templateUrl: './menu.component.html' ,
   selector: 'app-menu' ,
   styleUrls: ['./menu.component.scss']
+  host: {
+  '[@flyInOut]': 'true',
+  'style': 'display: block;'
+  },
+  animations: [
+    flyInOut(),
+    expand()
+  ]
 })
 export class MenuComponent implements OnInit {
 
