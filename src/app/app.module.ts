abedf185f590c13baadb5ac2dd49e58c5004cdbe
KushalBiyani <KushalBiyani@gmail.com>
2020-08-26 18:38:41 +0530

    2-4-4 Directive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -36,6 +36,7 @@ import { AppRoutingModule } from "./app-routing/app-routing.module";
 import { LoginComponent } from './login/login.component';
 import { MatDialogModule } from '@angular/material/dialog';
 import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
+import { HighlightDirective } from './directives/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -47,7 +48,8 @@ import { ProcessHTTPMsgService } from "./services/process-httpmsg.service";
     AboutComponent,
     HomeComponent,
     ContactComponent,
-    LoginComponent
+    LoginComponent,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -16,7 +16,7 @@ export class HomeComponent implements OnInit {
   dish: Dish;
   promotion: Promotion;
   leader: Leader;
-  dishErrMess: string
+  dishErrMess: string;
 
   constructor(private dishservice: DishService,
     private promotionservice: PromotionService,
@@ -24,8 +24,9 @@ export class HomeComponent implements OnInit {
     @Inject('BaseURL') public BaseURL) { }
 
   ngOnInit() {
-    this.dishservice.getFeaturedDish().subscribe(dish => this.dish = dish),
-    errmess => this.dishErrMess = <any>errmess);
+    this.dishservice.getFeaturedDish()
+    .subscribe(dish => this.dish = dish),
+    errmess => this.dishErrMess = <any>errmess );
     this.promotionservice.getFeaturedPromotion().subscribe(promotion => this.promotion = promotion);
     this.leaderservice.getFeaturedleader().subscribe(leader => this.leader = leader);
   }
